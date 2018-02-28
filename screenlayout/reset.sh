7750b33cdef0de0478266edea171d8eda0cf21bb
Gianluca Arbezzano <gianarb92@gmail.com>
2018-02-28 15:09:08 +0100

    Updated screenlayour
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- screenlayout/reset.sh
+++ screenlayout/reset.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off \
-    --output HDMI-1 --off \
-    --output DVI-I-1-1 --off \
-    --output DVI-I-2-2 --off \
-    --output eDP-1 --mode 1920x1080 --primary
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output HDMI-1 --off --output DP-1-3 --off --output DP-1-2 --off --output DP-1-1 --off
