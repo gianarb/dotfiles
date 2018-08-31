66f8e45eae7933581a8688124a252ed402ae78df
Gianluca Arbezzano <gianarb92@gmail.com>
2018-08-31 23:14:30 +0200

    update arandr setup
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- screenlayout/.screenlayout/reset.sh
+++ screenlayout/.screenlayout/reset.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output HDMI-1 --off --output DP-1-3 --off --output DP-1-2 --off --output DP-1-1 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --off --output DP2 --off
