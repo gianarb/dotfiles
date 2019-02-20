f29d6c1ee57548890b3041fdfc7ee6d845b538c5
Gianluca Arbezzano <gianarb92@gmail.com>
2019-02-20 01:24:51 +0100

    New monitor setup
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- screenlayout/.screenlayout/home_single.sh
+++ screenlayout/.screenlayout/home_single.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off
