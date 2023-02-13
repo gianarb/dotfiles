3896605e839c2ca63aeeb7c2a7550e018c4f8236
Gianluca Arbezzano <ciao@gianarb.it>
2023-02-13 10:19:04 +0100

    feat(nixos): try out autorandr

--- nixos/applications/i3.nix
+++ nixos/applications/i3.nix
@@ -3,6 +3,8 @@
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.lightdm.enableGnomeKeyring = true;
 
+  services.autorandr.enable = true;
+
   services.xserver = {
     enable = true;
 
@@ -21,6 +23,7 @@
       extraPackages = with pkgs; [
         xorg.libXcomposite # this should make drawing with Slak possible
         arandr
+        autorandr
         i3status # gives you the default i3 status bar
         i3lock #default i3 screen locker
         i3blocks #if you are planning on using i3blocks over i3status
